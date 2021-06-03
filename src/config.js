b1d18ce7680b1001931e60216289a3ecdc982331
Raihan Firmansyah <rehanfirmansyahr@gmail.com>
2021-06-03 22:54:56 +0700

    done

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const domain = "localhost:8000"
+const base_url = `http://${domain}/store/api/v1`
+const product_image_url = `http://${domain}/product_image/`
+const customer_image_url = `http://${domain}/image/`
+export { base_url, product_image_url, customer_image_url }
